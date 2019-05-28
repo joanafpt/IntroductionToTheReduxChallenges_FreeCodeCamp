ae939880c69369ee21285555ff18b1c626bd122d
Joana <joanajteixeira@outlook.com>
2019-05-28 16:02:40 +0100

    Redux: Define an Action Creator
    /*Define a function named actionCreator() that returns the action object when called.*/

---
+++ ReduxDefineanActionCreator.js
@@ -0,0 +1,15 @@
+//Redux: Define an Action Creator
+
+/*Define a function named actionCreator() that returns the action object when called.*/
+
+
+const action = {
+    type: 'LOGIN'
+  }
+  // Define an action creator here:
+  
+  function actionCreator(){
+      return action;
+  }
+  
+  actionCreator();
