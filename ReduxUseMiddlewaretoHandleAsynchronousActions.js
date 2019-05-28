f5b146c52ac7ee409049c7372a21367dad843595
Joana <joanajteixeira@outlook.com>
2019-05-28 16:06:20 +0100

    Redux: Use Middleware to Handle Asynchronous Actions
    /*Write both dispatches in the handleAsync() action creator. Dispatch requestingData() before the setTimeout() (the simulated API call). Then, after you receive the (pretend) data, dispatch the receivedData() action, passing in this data. Now you know how to handle asynchronous actions in Redux. Everything else continues to behave as before.*/

---
+++ ReduxUseMiddlewaretoHandleAsynchronousActions.js
@@ -0,0 +1,49 @@
+//Redux: Use Middleware to Handle Asynchronous Actions
+/*Write both dispatches in the handleAsync() action creator. Dispatch requestingData() before the setTimeout() (the simulated API call). Then, after you receive the (pretend) data, dispatch the receivedData() action, passing in this data. Now you know how to handle asynchronous actions in Redux. Everything else continues to behave as before.*/
+
+const REQUESTING_DATA = 'REQUESTING_DATA'
+const RECEIVED_DATA = 'RECEIVED_DATA'
+
+const requestingData = () => { return { type: REQUESTING_DATA } }
+const receivedData = (data) => { return { type: RECEIVED_DATA, users: data.users } }
+
+const handleAsync = () => {
+  return function (dispatch) {
+    /*Dispatch requestingData() before the setTimeout()*/
+    dispatch(requestingData())
+    setTimeout(function () {
+      let data = {
+        users: ['Jeff', 'William', 'Alice']
+      }
+      /*after you receive the (pretend) data, dispatch the receivedData() action, passing in this data.  */
+      dispatch(receivedData(data))
+    }, 2500);
+  }
+};
+
+const defaultState = {
+  fetching: false,
+  users: []
+};
+
+const asyncDataReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case REQUESTING_DATA:
+      return {
+        fetching: true,
+        users: []
+      }
+    case RECEIVED_DATA:
+      return {
+        fetching: false,
+        users: action.users
+      }
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(
+  asyncDataReducer,
+  Redux.applyMiddleware(ReduxThunk.default)
+);
